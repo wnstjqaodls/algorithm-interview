8ae8c8937c9891e1cf0f09d863af193cde6a7ae3
subkim <subkim@imbsystem.com>
2024-05-22 15:03:55 +0900

    solve P65 , 1h

--- src/programmers/lessons2/P65_240205_CountNumberFrequency.java
+++ src/programmers/lessons2/P65_240205_CountNumberFrequency.java
@@ -1,40 +1,46 @@
 package programmers.lessons2;
 
 public class P65_240205_CountNumberFrequency {
-  /*
-   * 팩토리얼의 끝자리 0의 개수를 반환하는 문제입니다.
-   * 예를 들어 5! = 5 * 4 * 3 * 2 * 1 = 120 입니다. 따라서 n에 주어진 대로 끝자리 0의 개수를 가진 결과를 리턴하는 solution 함수를 완성해주세요.
-   *
-   * 제한사항
-   * 1 ≤ n ≤ 3,628,800
-   *
-   * 입출력 예
-   * n       result
-   * 3628800    10
-   * 7          3
-   *
-   * 입출력 예 설명
-   * 입출력 예 #1
-   * 10! = 3,628,800으로 끝자리에 0은 10개 입니다.
-   *
-   * 입출력 예 #2
-   * 3! = 6, 4! = 24입니다. n이 7이므로, 7이하의 팩토리얼 중 3은 3개의 0을 리턴합니다.
-   */
+    /*
+     * i팩토리얼 (i!)은 1부터 i까지 정수의 곱을 의미합니다. 예를들어 5! = 5 * 4 * 3 * 2 * 1 = 120 입니다.
+     * 정수 n이 주어질 때 다음 조건을 만족하는 가장 큰 정수 i를 return 하도록 solution 함수를 완성해주세요.
+     * 제한사항
+     * 1 ≤ n ≤ 3,628,800
+     *
+     * 입출력 예
+     * n       result
+     * 3628800    10
+     * 7          3
+     *
+     *
+     * 목표 : 0 < n  ≤ 3,628,800 를 만족하는 i! 를 찾아야함
+     */
 
-  public static void main(String[] args) {
-    // 테스트 코드
-    System.out.println(solution(3628800)); // 양식에 맞게 결과를 확인하기 위한 출력문
-    System.out.println(solution(7)); // 양식에 맞게 결과를 확인하기 위한 출력문
-  }
+    public static void main (String[] args) {
+        // 테스트 코드
+        System.out.println(solution(100)); // 양식에 맞게 결과를 확인하기 위한 출력문
 
-  public static int solution(int n) {
-    int answer = 0;
+    }
 
-    // 여기에 로직을 구현해주세요.
-    // 1. n의 팩토리얼을 계산합니다.
+    public static int solution (int n) {
+        int answer = 1;
+        // 여기에 로직을 구현해주세요.
+        // 1. n의 팩토리얼을 계산합니다.
+        int factorNum = 1;
+        int i = 1;
 
-    // 2. 계산된 결과의 끝자리 0의 개수를 세어 answer에 할당합니다.
+        while (true) {
+            if (factorNum * (i + 1) > n) { //  다음 팩토리얼 값을 미리 계산해서 n 과 비교한다.
+                // 만약 다음 팩토리얼값이 n 보다 크면 현재 i 값이 최대 팩토리얼인것임.
+                break;
+            }
+            i++;
+            factorNum *= i;
 
-    return answer; // 계산된 끝자리 0의 개수를 반환합니다.
-  }
+        }
+
+        answer = i;
+
+        return answer; //
+    }
 }
